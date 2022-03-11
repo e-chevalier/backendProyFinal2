294f54b07ba5e9d8f02338a0fbea386676896451
e-chevalier <echevalier.dev@gmail.com>
2022-03-11 00:49:16 -0300

    Update CartService with CartsDaosMemory v1

--- components/carrito/services/carritoService.js
+++ components/carrito/services/carritoService.js
@@ -38,9 +38,7 @@ class Carrito {
       
         let cart = await cartsMemory.getById(id_cart)
         let prod = productsMemory.find( prod => prod.id == id_prod)
-        console.log(prod)
-        console.log(cart)
-
+      
         let response = {}
     
         if ( cart ) {
@@ -63,42 +61,23 @@ class Carrito {
     
         return response
     }
-    // async postCarritoProducto(id_cart, id_prod) {
-    //     console.log(`POST Carrito Add PROD  => idCart: ${id_cart} id_prod: ${id_prod} -- cartsRouters`)
-    //     let index_cart = cartsMemory.findIndex(cart => cart.id == id_cart)
-    //     let index_prod = products.findIndex( prod => prod.id == id_prod)
-    //     let response = {}
-    
-    //     if ( index_cart >= 0 ) {
-    //         if ( index_prod >= 0 ) {
-    //             cartsMemory[index_cart].products.push(products[index_prod])
-    //             //Save to file
-    //             cartsContainer.updateById(id_cart, cartsMemory[index_cart])
-    //             response = { status: "OK", description: `POST ADD ID_PROD: ${id_prod} INTO ID_CART: ${id_cart}`}
-    //             //cartsMemory[index_cart].products.forEach(e => console.log(e) )
-    //         } else {
-    //             response = { error: `Producto ID:${id_prod} no encontrado.` }
-    //         }  
-    //     } else {
-    //         response = { error: `Carrito ID:${id_cart} no encontrado.` }
-    //     }
-    
-    //     return response
-    // }
+  
 
     async deleteCarritoProducto(id_cart, id_prod) {
         console.log(`DELETE Productos IDPROD: ${id_prod}  FROM CART ID: ${id_cart} -- cartsRouters`)
-        let index_cart = cartsMemory.findIndex(cart => cart.id == id_cart)
+
+        let cart = await cartsMemory.getById(id_cart)
         let response = {}
         let index_prod = -1
 
-        if ( index_cart >= 0 ) {
-            index_prod = cartsMemory[index_cart].products.findIndex(prod => prod.id == id_prod)
-            if (index_prod >= 0 ) {
-                cartsMemory[index_cart].products.splice(index_prod, 1)
+        if ( cart ) {
+            index_prod = cart.products.findIndex(prod => prod.id == id_prod)
+            if (index_prod >= 0 ) {  // Prod id on cart
+                cart.products.splice(index_prod, 1) // Remove prod from cart
+                await cartsMemory.updateById(id_cart, {products: cart.products})
                 response = { status: "OK", description: `DELETE ID_PROD: ${id_prod} FROM ID_CART: ${id_cart}`}
                 //Save to file
-                cartsContainer.updateById(id_cart, cartsMemory[index_cart])
+                cartsContainer.updateById(id_cart, {products: cart.products})
                 //cartsMemory[index_cart].products.forEach(e => console.log(e) )
             } else {
                 response = { error: `Producto ID:${id_prod} no encontrado en el carrito ID:${id_cart} .` }
@@ -109,31 +88,6 @@ class Carrito {
     
         return(response)
     }
-
-
-    // async deleteCarritoProducto(id_cart, id_prod) {
-    //     console.log(`DELETE Productos IDPROD: ${id_prod}  FROM CART ID: ${id_cart} -- cartsRouters`)
-    //     let index_cart = cartsMemory.findIndex(cart => cart.id == id_cart)
-    //     let response = {}
-    //     let index_prod = -1
-
-    //     if ( index_cart >= 0 ) {
-    //         index_prod = cartsMemory[index_cart].products.findIndex(prod => prod.id == id_prod)
-    //         if (index_prod >= 0 ) {
-    //             cartsMemory[index_cart].products.splice(index_prod, 1)
-    //             response = { status: "OK", description: `DELETE ID_PROD: ${id_prod} FROM ID_CART: ${id_cart}`}
-    //             //Save to file
-    //             cartsContainer.updateById(id_cart, cartsMemory[index_cart])
-    //             //cartsMemory[index_cart].products.forEach(e => console.log(e) )
-    //         } else {
-    //             response = { error: `Producto ID:${id_prod} no encontrado en el carrito ID:${id_cart} .` }
-    //         }
-    //     } else {
-    //         response = { error: `Carrito ID:${id_cart} no encontrado.` }
-    //     }
-    
-    //     return(response)
-    // }
 
 }
 
