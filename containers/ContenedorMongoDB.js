8fda49c57cc9d9ccfa09d7d32fa7afcff8a6e878
e-chevalier <echevalier.dev@gmail.com>
2022-03-18 11:41:49 -0300

    Add find().lean() to getAll Container method

--- containers/ContenedorMongoDB.js
+++ containers/ContenedorMongoDB.js
@@ -64,7 +64,7 @@ class ContenedorMongoDB {
     async getAll() {
         try {
 
-            let res = await this.db.find({}, { '_id': 0, '__v': 0 })
+            let res = await this.db.find({}, { '_id': 0, '__v': 0 }).lean()
             return res
 
         } catch (error) {
